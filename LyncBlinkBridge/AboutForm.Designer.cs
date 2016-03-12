87ba6fc306bbd90ed8ad1d23a013501cfe7efc6a
renenulsch <nulsch@hotmail.com>
2016-03-12 09:01:07 +0100

    Initial commit
    Initial commit

---
+++ LyncBlinkBridge/AboutForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace LyncBlinkBridge
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAboutOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAboutOK
+            // 
+            this.buttonAboutOK.Location = new System.Drawing.Point(217, 69);
+            this.buttonAboutOK.Name = "buttonAboutOK";
+            this.buttonAboutOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonAboutOK.TabIndex = 0;
+            this.buttonAboutOK.Text = "OK";
+            this.buttonAboutOK.UseVisualStyleBackColor = true;
+            this.buttonAboutOK.Click += new System.EventHandler(this.buttonAboutOK_Click);
+            // 
+            // AboutForm
+            // 
+            this.AcceptButton = this.buttonAboutOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(304, 104);
+            this.Controls.Add(this.buttonAboutOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "About LyncBlinker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAboutOK;
+    }
+}
---
+++ LyncBlinkBridge/BlinkLyncConnector.cs
@@ -0,0 +1,245 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+using Microsoft.Lync.Model;
+
+using ThingM.Blink1;
+using ThingM.Blink1.ColorProcessor;
+
+namespace LyncBlinkBridge
+{
+    class BlinkLyncConnectorAppContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        private ContextMenuStrip trayIconContextMenu;
+        private ToolStripMenuItem closeMenuItem;
+        private ToolStripMenuItem aboutMenuItem;
+        private LyncClient lyncClient;
+        private Blink1 blink1 = new Blink1();
+        private bool isLyncIntegratedMode = true;
+
+        private Rgb colorAvailable = new Rgb(0, 150, 17);
+        private Rgb colorBusy = new Rgb(150, 0, 0);
+        private Rgb colorAway = new Rgb(150, 150, 0);
+        private Rgb colorOff = new Rgb(0, 0, 0);
+
+
+        public BlinkLyncConnectorAppContext()
+        {
+            Application.ApplicationExit += new System.EventHandler(this.OnApplicationExit);
+            blink1.Open();
+            InitializeComponent();
+            trayIcon.Visible = true;
+
+            //
+            // Lync Client Connection
+            //
+            GetLyncClient();
+
+        }
+
+        private void InitializeComponent()
+        {
+            trayIcon = new NotifyIcon();
+
+            trayIcon.BalloonTipIcon = ToolTipIcon.Info;
+            trayIcon.BalloonTipText =
+              "I noticed that you double-clicked me! What can I do for you?";
+            trayIcon.BalloonTipTitle = "I've not much to say!";
+            trayIcon.Text = "Lync Status:";
+
+
+            //The icon is added to the project resources.
+            //Here I assume that the name of the file is 'TrayIcon.ico'
+            trayIcon.Icon = Properties.Resources.TrayIcon;
+
+            //Optional - handle doubleclicks on the icon:
+            trayIcon.DoubleClick += TrayIcon_DoubleClick;
+
+            // 
+            // TrayIconContextMenu
+            // 
+            trayIconContextMenu = new ContextMenuStrip();
+            trayIconContextMenu.SuspendLayout();
+            trayIconContextMenu.Name = "TrayIconContextMenu";
+
+            this.trayIconContextMenu.Items.Add("Available", null, new EventHandler(AvailableMenuItem_Click));
+            this.trayIconContextMenu.Items.Add("Busy", null, new EventHandler(BusyMenuItem_Click));
+            this.trayIconContextMenu.Items.Add("Away", null, new EventHandler(AwayMenuItem_Click));
+            this.trayIconContextMenu.Items.Add("Off", null, new EventHandler(OffMenuItem_Click));
+
+            // Separation Line
+            this.trayIconContextMenu.Items.Add(new ToolStripSeparator());
+
+            // About Form Line
+            aboutMenuItem = new ToolStripMenuItem();
+            this.aboutMenuItem.Name = "aboutMenuItem";
+            this.aboutMenuItem.Text = "About";
+            this.aboutMenuItem.Click += new EventHandler(this.aboutMenuItem_Click);
+            this.trayIconContextMenu.Items.Add(aboutMenuItem);
+
+            // Separation Line
+            this.trayIconContextMenu.Items.Add(new ToolStripSeparator());
+
+            // 
+            // CloseMenuItem
+            // 
+            closeMenuItem = new ToolStripMenuItem();
+            this.closeMenuItem.Name = "CloseMenuItem";
+            //this.closeMenuItem.Size = new Size(152, 22);
+            this.closeMenuItem.Text = "Exit";
+            this.closeMenuItem.Click += new EventHandler(this.CloseMenuItem_Click);
+            this.trayIconContextMenu.Items.Add(closeMenuItem);
+
+            trayIconContextMenu.ResumeLayout(false);
+            trayIcon.ContextMenuStrip = trayIconContextMenu;
+
+
+        }
+
+        void GetLyncClient()
+        {
+            try
+            {
+                lyncClient = LyncClient.GetClient();
+                lyncClient.StateChanged += lyncClient_StateChanged;
+                
+                if (lyncClient.State == ClientState.SignedIn)
+                    lyncClient.Self.Contact.ContactInformationChanged += Contact_ContactInformationChanged;
+    
+                SetCurrentContactState();
+            }
+            catch (ClientNotFoundException)
+            {
+                SetLyncIntegrationMode(false);
+                trayIcon.ShowBalloonTip(1000, "Error", "Lync Client not started. Running in manual mode now. Please use the context menu to change your blink color", ToolTipIcon.Warning);
+            }
+            catch (Exception e)
+            {
+                trayIcon.ShowBalloonTip(1000, "Error", "Something went wrong by getting your Lync status. Running in manual mode now. Please use the context menu to change your blink color", ToolTipIcon.Warning);
+            }
+        }
+
+        void SetLyncIntegrationMode(bool isLyncIntegrated)
+        {
+            isLyncIntegratedMode = isLyncIntegrated;
+            if (isLyncIntegratedMode)
+            {
+            }
+            else
+            {
+            }
+        }
+
+        void SetCurrentContactState()
+        {
+            Rgb newColor = new Rgb(0,0,0);
+            if (lyncClient.State == ClientState.SignedIn)
+            {
+                ContactAvailability currentAvailability = (ContactAvailability)lyncClient.Self.Contact.GetContactInformation(ContactInformationType.Availability);
+                switch (currentAvailability)
+                {
+                    case ContactAvailability.Busy:
+                        newColor = colorBusy;
+                        break;
+                    case ContactAvailability.Free:
+                        newColor = colorAvailable;
+                        break;
+                    case ContactAvailability.Away:
+                        newColor = colorAway;
+                        break;
+                    default:
+                        break;
+                }
+
+                SetBlink1State(newColor);
+            }
+        }
+
+        void SetBlink1State(Rgb color)
+        {
+            blink1.SetColor(color);
+        }
+
+        void lyncClient_StateChanged(object sender, ClientStateChangedEventArgs e)
+        {
+            switch (e.NewState)
+            {
+                case ClientState.Initializing:
+                    break;
+                case ClientState.Invalid:
+                    break;
+                case ClientState.ShuttingDown:
+                    break;
+                case ClientState.SignedIn:
+                    lyncClient.Self.Contact.ContactInformationChanged += Contact_ContactInformationChanged;
+                    SetCurrentContactState();
+                    break;
+                case ClientState.SignedOut:
+                    trayIcon.ShowBalloonTip(1000, "", "You signed out in Lync. Switching to manual mode.", ToolTipIcon.Info);
+                    break;
+                case ClientState.SigningIn:
+                    break;
+                case ClientState.SigningOut:
+                    break;
+                case ClientState.Uninitialized:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void Contact_ContactInformationChanged(object sender, ContactInformationChangedEventArgs e)
+        {
+            if (e.ChangedContactInformation.Contains(ContactInformationType.Availability))
+            {
+                SetCurrentContactState();
+            }
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            //Cleanup so that the icon will be removed when the application is closed
+            trayIcon.Visible = false;
+            blink1.Close();
+        }
+        
+        private void TrayIcon_DoubleClick(object sender, EventArgs e)
+        {
+            //Here you can do stuff if the tray icon is doubleclicked
+            trayIcon.ShowBalloonTip(10000);
+        }
+
+        private void CloseMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm about = new AboutForm();
+            about.ShowDialog();
+        }
+
+        private void OffMenuItem_Click(object sender, EventArgs e)
+        {
+            SetBlink1State(colorOff);
+        }
+
+        private void AwayMenuItem_Click(object sender, EventArgs e)
+        {
+            SetBlink1State(colorAway);
+        }
+
+        private void BusyMenuItem_Click(object sender, EventArgs e)
+        {
+            SetBlink1State(colorBusy);
+        }
+
+        private void AvailableMenuItem_Click(object sender, EventArgs e)
+        {
+            SetBlink1State(colorAvailable);
+        }
+    }
+}
