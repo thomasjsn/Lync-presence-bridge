19b486d4ca28e942e6ad198ca2edaad369965aaf
Thomas Jensen <HebronNor@users.noreply.github.com>
2016-05-23 13:34:22 +0200

    Quick and dirty repo URL update.

--- LyncPresenceBridge/AboutForm.cs
+++ LyncPresenceBridge/AboutForm.cs
@@ -22,7 +22,7 @@ namespace LyncPresenceBridge
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/uCtrlDev/Lync-Blink-Bridge");
+            System.Diagnostics.Process.Start("https://github.com/uCtrlDev/Lync-presence-bridge");
         }
     }
 }
